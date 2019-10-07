5dff3a1b5f8fca9f5b42c422635c931249d32c0c
rafaeldtx <rafael991_@hotmail.com>
2019-10-07 10:16:56 -0300

    Add test for duplicated recipe_types

--- spec/features/admin_register_recipe_type_spec.rb
+++ spec/features/admin_register_recipe_type_spec.rb
@@ -11,4 +11,18 @@ feature 'Admin register recipe type' do
     # Assert
     expect(page).to have_content('Sobremesa')
   end
+
+  scenario 'and not insert duplicated' do
+    recipe_type = RecipeType.create(name: "Sobremesa")
+    user = User.create!(email: "teste@teste.com", password: "teste123")
+    
+    login_as(user, :scope => :user)
+    visit root_path
+    click_on 'Enviar tipo da receita'
+    fill_in 'Nome', with: 'Sobremesa'
+    click_on 'Enviar'
+
+    expect(page).to have_content('Tipo de receita já cadastrada')
+    expect(current_path).not_to have_content('Sobremesa')
+  end
 end
