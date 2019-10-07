6addd4833e1d7ae0dbdc04208efa4228137441fd
Derick Rosa <derickedu@gmail.com>
2019-10-07 16:00:01 -0300

    Inclusao de migration enum de user_type

---
+++ spec/requests/admin_logged_in_to_approve_recipe_spec.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'Admin logged in to approve recipe' do
+  it 'access list' do
+    recipe_type = RecipeType.create(name: 'Sobremesa')
+    user = User.create!(email: 'teste@teste.com', password: 'teste123',
+                        user_type: :customer)
+
+    Recipe.create(title: 'Bolo de Cenoura', difficulty: 'Médio',
+                  recipe_type: recipe_type, cuisine: 'Brasileira',
+                  cook_time: 50,
+                  ingredients: 'Farinha, açucar, cenoura', user: user,
+                  cook_method: 'Cozinhe a cenoura, corte em pedaços pequenos, misture com o restante dos ingredientes', status: :pending)
+
+    login_as(user, scope: :user)
+
+    get approve_list_recipes_path
+    expect(response).to redirect_to(root_path)
+  end
+
+  it 'access approve' do
+    recipe_type = RecipeType.create(name: 'Sobremesa')
+    user = User.create!(email: 'teste@teste.com', password: 'teste123',
+                        user_type: :customer)
+
+    pending_recipe = Recipe.create(title: 'Bolo de Cenoura', difficulty: 'Médio',
+                                   recipe_type: recipe_type, cuisine: 'Brasileira',
+                                   cook_time: 50,
+                                   ingredients: 'Farinha, açucar, cenoura', user: user,
+                                   cook_method: 'Cozinhe a cenoura, corte em pedaços pequenos, misture com o restante dos ingredientes', status: :pending)
+
+    login_as(user, scope: :user)
+
+    get approve_recipe_path(pending_recipe)
+    expect(response).to redirect_to(root_path)
+    expect(pending_recipe).to be_pending
+  end
+
+  it 'access reprove' do
+    recipe_type = RecipeType.create(name: 'Sobremesa')
+    user = User.create!(email: 'teste@teste.com', password: 'teste123',
+                        user_type: :customer)
+
+    pending_recipe = Recipe.create(title: 'Bolo de Cenoura', difficulty: 'Médio',
+                                   recipe_type: recipe_type, cuisine: 'Brasileira',
+                                   cook_time: 50,
+                                   ingredients: 'Farinha, açucar, cenoura', user: user,
+                                   cook_method: 'Cozinhe a cenoura, corte em pedaços pequenos, misture com o restante dos ingredientes', status: :pending)
+
+    login_as(user, scope: :user)
+
+    get reprove_recipe_path(pending_recipe)
+    expect(response).to redirect_to(root_path)
+    expect(pending_recipe).to be_pending
+  end
+end
